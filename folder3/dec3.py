a68905942cc6f481c0c4695fc1482feecefc0ec5
Bruno <b.nadj11@gmail.com>
2026-04-27 17:50:24 +0200

    Add

---
+++ folder3/dec3.py
@@ -0,0 +1,16 @@
+print("This is third branch")
+
+def double_number(func):
+    def wrapper(*args, **kwargs):
+        return func(*args)*2
+    return wrapper
+
+
+@double_number
+def sum_numbers(*args):
+    ss = 0
+    for arg in args:
+        ss+=arg
+    return ss
+
+print(sum_numbers(1,5,6))
